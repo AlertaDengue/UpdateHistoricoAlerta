5a2d4201550a2819ad98ce589fdfd3e68103e216
esloch <es.loch@gmail.com>
2021-09-06 05:39:31 -0300

    Create task to remove sql script

--- enter_filename.sh
+++ enter_filename.sh
@@ -14,9 +14,9 @@ read -p "Epidemic YearWeek: " YEARWEEK
 echo DISEASE=${DISEASE} > .var_file_names
 echo YEARWEEK=${YEARWEEK} >> .var_file_names
 
-for file in sql/*output.sql
+for file in sql/*${DISEASE}.sql
 do
-  cp "$file" "/tmp/${file/output.sql/update_hist_alerta_${YEARWEEK}_${DISEASE}.sql}"
+  cp "$file" "/tmp/${file/${DISEASE}.sql/update_hist_alerta_${YEARWEEK}_${DISEASE}.sql}"
   echo -e "Successfully renamed the file!"
 done
 
